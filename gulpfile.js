931be35e32faa3c828e064b6d47213cbdecbe19a
neronmoon <alistar.neron@gmail.com>
2016-08-07 17:19:32 +0200

    nice and clean home page

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,18 @@ var elixir = require('laravel-elixir');
 require('laravel-elixir-vueify');
 
 elixir(function (mix) {
+
+    var node_modules = '../../../node_modules/';
+
+    mix.copy('node_modules/font-awesome/fonts', 'public/fonts');
+
     mix.browserify('app.js');
     mix.browserify('welcome.js');
-    var node_modules = '../../../node_modules/';
+
     mix.sass([
         'app.scss'
     ], 'public/css/app.css');
+
     mix.sass([
         'welcome.scss'
     ], 'public/css/welcome.css');
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,31 @@
     <link rel="stylesheet" href="/css/welcome.css"/>
 </head>
 <body>
-<div id="app" class="container">
-    <welcome></welcome>
+<div class="container">
+    <div class="content">
+        <div class="triangle">△</div>
+        <div class="title">IRIDIUM</div>
+        <div class="form row">
+            <div class="strike">
+                <span>Get in via email</span>
+            </div>
+            <div class="col-md-8">
+                <input type="email" placeholder="Email" class="form-control">
+            </div>
+            <div class="col-md-4">
+                <a href="#" class="btn btn-lg btn-dark">Get In</a>
+            </div>
+        </div>
+        <div class="social row">
+            <div class="strike">
+                <span>or via social links</span>
+            </div>
+            <ul class="list-inline">
+                <li><a href="#"><i class="fa fa-vk"></i></a></li>
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+            </ul>
+        </div>
+    </div>
 </div>
-<script src="/js/welcome.js"></script>
 </body>
 </html>
