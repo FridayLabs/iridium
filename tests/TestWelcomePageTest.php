61998aa87c2584b7aa840c62fefebfaa83ede150
neronmoon <alistar.neron@gmail.com>
2016-08-18 23:21:12 +0200

    very first test just for installing test env

--- tests/TestWelcomePageTest.php
+++ tests/TestWelcomePageTest.php
@@ -4,16 +4,15 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class TestWelcomePage extends TestCase
 {
     /**
-     * A basic functional test example.
+     * A basic test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testTitleIsCorrect()
     {
-        $this->visit('/')
-             ->see('Laravel 5');
+        $this->visit('/')->see('Iridium');
     }
 }
